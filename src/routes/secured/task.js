ba7debfcc292f483a401f6dcc6cea9866ab97ee7
Chahine-tech <chahine.benahcen@gmail.com>
2021-03-14 15:47:28 +0100

    remove task

--- src/routes/secured/task.js
+++ src/routes/secured/task.js
@@ -28,10 +28,66 @@ api.post('/add', async (req, res) => {
       }
 })
 
-//faire le delete
-api.delete('/delete', async (req, res) => {
+/*api.put('/update/:id', async (req, res) => {
+  try {
+    const id = parseInt(req.params.id, 10)
 
+    const prisma = new PrismaClient()
+    const user = await prisma.user.findFirst({
+      where: {
+        id
+      }
+    })
+
+    if (!user) {
+      return res.status(BAD_REQUEST.status).json({ error: `User ${id} doesn't exist` })
+    }
+
+    const acceptedFields = ['firstname', 'lastname', 'gender']
+    let data = {}
+    for (const key of acceptedFields) {
+      if (req.body[key]) {
+        data[key] = req.body[key]
+      }
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        id
+      },
+      data
+    })
+
+    res.json({ data: { user: updatedUser } })
+  } catch (err) {
+    res.status(BAD_REQUEST.status).json({ error: err.message })
+  }
+})
+*/
+
+
+
+
+
+api.delete('/delete/:id', async (req, res) => {
+
+  const prisma = new PrismaClient()
+
+  try {
+    const id = parseInt(req.params.id, 10)
+    
+        const task = await prisma.task.delete({
+        where: {
+            id,
+        }
+    })
+
+    res.json({ data: { task } })
+} catch (err) {
+    res.status(400).json({ error: err.message })
+}
 })
 
 
+  
 export default api
